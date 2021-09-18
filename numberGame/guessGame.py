79f88b5c4427645da936a5143eb6ebdbc5199d1d
Mia Thomas <miathomas1102@gmail.com>
2021-09-18 14:32:48 +0100

    Number Game completed

--- numberGame/guessGame.py
+++ numberGame/guessGame.py
@@ -1,6 +1,3 @@
-''' The main goal of the project is to create a program that randomly select a number in a range then the user has to guess the number. user has three chances to guess the number if he guess correct then a message print saying “you guess right “otherwise a negative message prints.
-Topics: random module, for loop, f strings '''
-
 import random
 print("Welcome to the Number Guessing Game, lets begin with numbers between 1 and 10!")
 userInput=()
