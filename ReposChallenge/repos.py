2f96a105b9d5ba24a90b143fafbab8e2245d43ea
Mia Thomas <miathomas1102@gmail.com>
2021-09-20 06:33:09 +0100

    Declaring Challenge

---
+++ ReposChallenge/repos.py
@@ -0,0 +1,9 @@
+''' A program that prompts for a list of repositories
+
+Add each item to an array
+
+Loop through the array and using the GitHub API, check if those repositories already exist and print to the user if the repo exists or not
+
+Bonus ( not sure if it can be done with API but probably can ), when checking if repo exists, if it doesn’t then ask the user if they want it to be created, if they do then create the repo '''
+
+print("hello world")
